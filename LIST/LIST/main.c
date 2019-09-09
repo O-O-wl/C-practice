97d4a500613ca03a529007ca6f2ec35796ac3264
부엉이 <o.o.wl.soft@gmail.com>
2019-09-10 01:04:16 +0900

    feat: Implement  LinkedList - fin

--- LIST/LIST/main.c
+++ LIST/LIST/main.c
@@ -9,30 +9,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "LinkedList.h"
-#include "DoubleLinkedList.h"
 
 
-//int main(int argc, const char * argv[]) {
-//    Node* list = NULL;
-//    Node* node1 = createNode(1);
-//    Node* node2 = createNode(2);
-//    Node* node3 = createNode(3);
-//
-//    appendNode(&list, node1);
-//    appendNode(&list, node2);
-//    appendNode(&list, node3);
-//    print(list);
-//    Node* searchNode = get(list, 2);
-//    printf("\n%d\n", (searchNode->data));
-//    //    delete(list, 2);
-////    removeNode(&list, node3);
-////    print(list);
-//    int countt = count(&list);
-//    printf("\n갯수: %d\n",countt);
-//    return 0;
-//}
-
-int main(int argc, const char* argv[]) {
+int main(int argc, const char * argv[]) {
+    Node* list = NULL;
+    Node* node1 = createNode(1);
+    Node* node2 = createNode(2);
+    Node* node3 = createNode(3);
     
+    appendNode(&list, node1);
+    appendNode(&list, node2);
+    appendNode(&list, node3);
+    print(list);
+    Node* searchNode = get(list, 2);
+    printf("\n%d\n", (searchNode->data));
+//    delete(list, 2);
+    removeNode(&list, node3);
+    print(list);
+    int countt = count(&list);
+    printf("\n갯수: %d\n",countt);
     return 0;
 }
+
